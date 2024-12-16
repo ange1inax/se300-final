9f689c584e83d6dad59ae745bee502b61796f695
Angelina Dieu <96335291+ange1inax@users.noreply.github.com>
2024-12-15 23:37:57 -0500

    Update LedgerRestController.java

--- src/main/java/com/se300/ledger/controller/LedgerRestController.java
+++ src/main/java/com/se300/ledger/controller/LedgerRestController.java
@@ -3,7 +3,6 @@ package com.se300.ledger.controller;
 import com.se300.ledger.model.Account;
 import com.se300.ledger.service.LedgerException;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 
 public class LedgerRestController {
 
